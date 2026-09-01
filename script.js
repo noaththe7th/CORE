68be4f70af4d4562763ed378c886b7c88d3e36cc
noahthe7th <matandanoah1@gmail.com>
2026-09-01 16:26:15 +0200

    intial  commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById
+("signup-form").addEventListener("submit", function(event) {
+ 
+  
+});
+document.getElementById("login-form").addEventListener("submit", function(event) {
+  ;
+ 
+});
